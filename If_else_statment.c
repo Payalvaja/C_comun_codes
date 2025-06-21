82ff33593c8e74127086776869d37cf074a2929a
Payalvaja <payalvaja80@gmail.com>
2025-06-21 18:40:45 +0530

    Create If_else_statment.c

---
+++ If_else_statment.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+int value1=10;
+int value2=0;
+printf("Enter  value1");
+scanf("%d",& value1);
+printf("Enter value2");
+scanf ("%d", &value2);
+if(value1 <value2){
+printf("value1 is less than to value2\n");
+}
+else if (value1>value2){
+printf("value1 is great than to value2\n");
+}
+else{
+printf("1 is true 0 is false");
+}
+return 0;
+
+
+}
